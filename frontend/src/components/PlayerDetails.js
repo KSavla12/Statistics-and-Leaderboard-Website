f8320424e6a1ab96feac91027d00c9baaec0e932
KSavla12 <119696161+KSavla12@users.noreply.github.com>
2023-06-14 11:53:57 +0100

    Update PlayerDetails.js

--- frontend/src/components/PlayerDetails.js
+++ frontend/src/components/PlayerDetails.js
@@ -10,9 +10,6 @@ const PlayerDetails = ({ playerid, player_data }) => {
   let [loading, setLoading] = useState(true);
 
   const url = async () => {
-    // const response = await fetch("http://aligulac.com/api/v1/player/?apikey=DFwWEKaprPJIMgYBcWp2&format=json/&id__iexact=" + playerid)
-    // const response = await fetch("https://api.esportsearnings.com/v0/LookupPlayerById?apikey=ce7ed365411cc853d8184a2fc6aa22b1fdc34eb9b300db3d47204a1a024d573f&playerid=5513")
-    // setExample(await response.json())
     setLoading(false)
   }
 
@@ -23,14 +20,7 @@ const PlayerDetails = ({ playerid, player_data }) => {
   let name = example.NameFirst + example.NameSecond
 
   if (loading) return null
- 
 
-  // let race = [Terran, ' Terran']
-  // if (example.objects[0].race === 'Z'){
-  //   race = [Zerg, ' Zerg']
-  // } else if(example.objects[0].race === 'P') {
-  //   race = [Protoss, ' Protoss']
-  // }
 
   let race = Terran
   if (player_data.player_race === 'Zerg'){
@@ -49,34 +39,6 @@ const PlayerDetails = ({ playerid, player_data }) => {
   return (
 
       <tbody>
-        {/* <tr>
-          <td className="td-left-col">Race</td>
-          <td><img src={race[0]}/>{race[1]}</td>
-        </tr>
-        <tr>
-          <td className="td-left-col">Country</td>
-          <td><img src={`https://flagcdn.com/16x12/${example.objects[0].country.toLowerCase()}.png`}></img>
-          &nbsp;
-          {countryCode1(example.objects[0].country)}</td>
-        </tr>
-        <tr>
-          <td className="td-left-col">Full name	</td>
-          <td>{example.objects[0].name}</td>
-        </tr>
-        <tr>
-          <td className="td-left-col">Birthday</td>
-          <td>{example.objects[0].birthday}</td>
-        </tr>
-        <tr>
-          <td className="td-left-col">Team</td>
-          <td>{example.objects[0].current_teams[0].team.name}</td>
-        </tr> 
-        <tr>
-          <td className="td-left-col">Total earnings</td>
-          <td>{formatter.format(example.objects[0].total_earnings)}</td> 
-        </tr>
-        */}
-
 
         <tr>
           <td className="td-left-col">Race</td>
